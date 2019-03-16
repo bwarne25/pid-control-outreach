4e7fd00c3934c3ac01b1fabec94061360bb88cdb
brandon <brandon@redpeppersoftware.com>
2019-03-16 16:35:30 -0600

    cleaned up arduino_helper

--- arduino_helper.py
+++ arduino_helper.py
@@ -1,48 +1,51 @@
-import time  # Required to timestamp & delay functions
+import time
 import numpy as np
 
 from pyfirmata import Arduino, util, STRING_DATA
 
 portName = 'COM5'
 
-arduino = Arduino(portName)  
-it = util.Iterator(arduino)
-it.start()
-start_time = time.time()
-input_pin = arduino.get_pin('a:0:i') #analog, input to the computer
-output_pin = arduino.get_pin('d:6:p') #digital, PWM mode, from the computer
-switch_pin = arduino.get_pin('d:9:i') #digital, input to the computer (switch-state)
+try:
+    arduino = Arduino(portName)
+    it = util.Iterator(arduino)
+    it.start()
+    start_time = time.time()
+    input_pin = arduino.get_pin('a:0:i')
+    output_pin = arduino.get_pin('d:6:p')
+    switch_pin = arduino.get_pin('d:9:i')
+except:
+    pass
 
 
 def get_temperature():
-    temperature = calculate_temperature(read_input_pin())
-    return temperature
+    try:
+        temperature = calculate_temperature(read_input_pin())
+        return temperature
+    except:
+        return 25
+
 
 def read_input_pin():
-    return read_voltage_fast(input_pin)
-def read_voltage(pin):
-    """
-        Returns the voltage of the pin passed in (between 0 and 5 Volts)
-        It takes a number of measurements, then averages them to reduce noise
-    """
-    outputs = []
-    for i in range(2):
-        time.sleep(.05)
-        outputs.append(pin.read())
-    return 5.0 * np.average(outputs)
+    try:
+        return read_voltage_fast(input_pin)
+    except:
+        return 1
+
+
 def read_voltage_fast(pin):
     """
         Returns the voltage of the pin passed in (between 0 and 5 Volts)
         It takes a single of measurement
     """
     return 5.0 * pin.read()
-    
-def is_switch_on():
-    switch_voltage = read_voltage_fast(switch_pin)
-    return switch_voltage > 1
 
-def update_duty_cycle(DC): 
-    output_pin.write(DC)
+
+def update_duty_cycle(DC):
+    try:
+        output_pin.write(DC)
+    except:
+        pass
+
 
 def calculate_temperature(signal1):
     """
@@ -54,5 +57,9 @@ def calculate_temperature(signal1):
                    2.620131e-06*R_ratio**2 + 6.383091e-08*R_ratio**3))
     return (T_Kelvin)-273.15
 
-time.sleep(2)
-update_duty_cycle(0)
+
+try:
+    time.sleep(2)
+    update_duty_cycle(0)
+except:
+    pass
