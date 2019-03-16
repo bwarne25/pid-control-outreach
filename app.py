1981af10a8c0dcf956c27c74d4969028a87da1c9
brandon <brandon@redpeppersoftware.com>
2019-03-16 16:27:19 -0600

    Configured dash to serve scripts locally

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ start_time = time.time()
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 server = app.server
 app.config.suppress_callback_exceptions = True
 connected = False
